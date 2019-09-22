7a7773f166bd158d0734bca54b7a86aaa0b3640e
0xd34df00d <0xd34df00d@gmail.com>
2019-09-22 17:20:11 -0400

    Define the type for options

---
+++ src/Clang/Format/Descr.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE DataKinds, TypeFamilies #-}
+
+module Clang.Format.Descr where
+
+import qualified Data.Text as T
+import Data.Void
+import Numeric.Natural
+
+data ConfigItemType = ParsedInfo | SupportedInfo | Instance
+
+type family ConfigTypeData f ty where
+  ConfigTypeData 'ParsedInfo _ = ()
+  ConfigTypeData 'SupportedInfo Void = Void
+  ConfigTypeData 'SupportedInfo _ = ()
+  ConfigTypeData 'Instance ty = ty
+
+data ConfigTypeT f
+  = CTInt (ConfigTypeData f Int)
+  | CTUnsigned (ConfigTypeData f Natural)
+  | CTBool (ConfigTypeData f Bool)
+  | CTString (ConfigTypeData f Void)
+  | CTStringVec (ConfigTypeData f Void)
+  | CTEnum { variants :: [T.Text], enumValue :: ConfigTypeData f T.Text }
+
+type ConfigType = ConfigTypeT 'ParsedInfo
+type ConfigTypeSupported = ConfigTypeT 'ParsedInfo
+type ConfigValue = ConfigTypeT 'Instance
+
+data ConfigItemT f = ConfigItem
+  { name :: T.Text
+  , typ :: ConfigTypeT f
+  }
+
+type ConfigItem = ConfigItemT 'ParsedInfo
+type ConfigItemSupported = ConfigItemT 'SupportedInfo
+type ConfigInstance = ConfigItemT 'Instance
