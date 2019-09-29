5a3c52bfd3197c5cba8435715ac5d7b40c8c9f6d
0xd34df00d <0xd34df00d@gmail.com>
2019-09-29 09:57:05 -0400

    Stub defaults parsing

---
+++ src/Clang/Format/DescrFiller.hs
@@ -0,0 +1,62 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE DeriveGeneric, DeriveAnyClass #-}
+{-# LANGUAGE RecordWildCards #-}
+
+module Clang.Format.DescrFiller
+( fillConfigItemsIO
+) where
+
+import qualified Data.Text as T
+import qualified Data.HashMap.Strict as HM
+import Control.Monad.Except
+import Control.Monad.Except.CoHas
+import Data.Bifunctor
+import Data.Scientific
+import Data.Yaml
+import GHC.Generics
+
+import Clang.Format.Descr
+
+data FillError
+  = YamlParseError ParseException
+  | YamlAnalysisError YamlAnalysisError
+  deriving (Generic, CoHas ParseException, CoHas YamlAnalysisError)
+
+liftEitherCoHas :: (MonadError sum m, CoHas e sum) => Either e a -> m a
+liftEitherCoHas = liftEither . first inject
+
+fillConfigItemsIO :: (MonadIO m, MonadError e m, CoHas ParseException e, CoHas YamlAnalysisError e)
+                  => [ConfigItemT 'Supported]
+                  -> FilePath
+                  -> m [ConfigItemT 'WithDefault]
+fillConfigItemsIO supported path = liftIO (decodeFileEither path) >>= liftEitherCoHas >>= fillConfigItems supported
+
+data YamlAnalysisError
+  = YamlNotAnObject
+  | ValueNotFound T.Text
+  | IncompatibleValue (ConfigTypeT 'Supported) Value
+  deriving (Show)
+
+fillConfigItems :: (MonadError e m, CoHas YamlAnalysisError e)
+                => [ConfigItemT 'Supported] -> Value -> m [ConfigItemT 'WithDefault]
+fillConfigItems supported (Object fields) = mapM (fillConfigItem fields) supported
+fillConfigItems _ _ = throwError $ inject YamlNotAnObject
+
+fillConfigItem :: (MonadError e m, CoHas YamlAnalysisError e)
+               => Object -> ConfigItemT 'Supported -> m (ConfigItemT 'WithDefault)
+fillConfigItem obj ConfigItem { .. } = do
+  yamlVal <- case HM.lookup name obj of
+                  Just val -> pure val
+                  Nothing -> throwError $ inject $ ValueNotFound name
+  val <- case (typ, yamlVal) of
+              (CTInt _, Number num)
+                  | Just int <- toBoundedInteger num -> pure $ CTInt int
+              (CTUnsigned _, Number num)
+                  | Just int <- toBoundedInteger num :: Maybe Int
+                  , int >= 0 -> pure $ CTUnsigned $ fromIntegral int
+              (CTBool _, Bool b) -> pure $ CTBool b
+              (CTEnum vars _, String s)
+                  | s `elem` vars -> pure $ CTEnum vars s
+              _ -> throwError $ inject $ IncompatibleValue typ yamlVal
+  pure ConfigItem { name = name, typ = val }
