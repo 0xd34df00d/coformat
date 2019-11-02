2978e59fc2a7d56d0cdbddbe25125ce9c326805a
0xd34df00d <0xd34df00d@gmail.com>
2019-11-02 13:00:07 -0400

    Bump to the new can-i-haz

--- src/Clang/Format/DescrFiller.hs
+++ src/Clang/Format/DescrFiller.hs
@@ -10,9 +10,7 @@ module Clang.Format.DescrFiller
 
 import qualified Data.Text as T
 import qualified Data.HashMap.Strict as HM
-import Control.Monad.Except
 import Control.Monad.Except.CoHas
-import Data.Bifunctor
 import Data.Either.Combinators
 import Data.Scientific
 import Data.Yaml
@@ -25,15 +23,12 @@ data FillError
   | YamlAnalysisError YamlAnalysisError
   deriving (Show, Generic, CoHas ParseException, CoHas YamlAnalysisError)
 
-liftEitherCoHas :: (MonadError sum m, CoHas e sum) => Either e a -> m a
-liftEitherCoHas = liftEither . first inject
-
 fillConfigItemsIO :: (MonadIO m, MonadError e m, CoHas ParseException e, CoHas YamlAnalysisError e)
                   => [ConfigItemT 'Supported]
                   -> FilePath
                   -> m [ConfigItemT 'WithDefault]
 fillConfigItemsIO supported path = liftIO (decodeFileEither path)
-                               >>= liftEitherCoHas
+                               >>= liftEither
                                >>= extractMap
                                >>= braceWrappingKludge
                                >>= fillConfigItems supported
@@ -46,14 +41,14 @@ data YamlAnalysisError
 
 extractMap :: (MonadError e m, CoHas YamlAnalysisError e) => Value -> m Object
 extractMap (Object fields) = pure fields
-extractMap _ = throwError $ inject $ YamlNotAnObject "Top-level value is not an object"
+extractMap _ = throwError $ YamlNotAnObject "Top-level value is not an object"
 
 braceWrappingKludge :: (MonadError e m, CoHas YamlAnalysisError e) => Object -> m Object
 braceWrappingKludge fields = do
-  bwVal <- liftEitherCoHas $ maybeToRight (ValueNotFound bwField) $ HM.lookup bwField fields
+  bwVal <- liftEither $ maybeToRight (ValueNotFound bwField) $ HM.lookup bwField fields
   bwValObj <- case bwVal of
                    Object obj -> pure obj
-                   _ -> throwError $ inject $ YamlNotAnObject "Brace wrapping value is not an object"
+                   _ -> throwError $ YamlNotAnObject "Brace wrapping value is not an object"
   let bwSubfields = HM.fromList [ (bwField <> "." <> key, val)
                                 | (key, val) <- HM.toList bwValObj
                                 ]
@@ -68,7 +63,7 @@ fillConfigItems supported fields = mapM fillConfigItem supported
     fillConfigItem ConfigItem { .. } = do
       yamlVal <- case HM.lookup name fields of
                       Just val -> pure val
-                      Nothing -> throwError $ inject $ ValueNotFound name
+                      Nothing -> throwError $ ValueNotFound name
       val <- case (typ, yamlVal) of
                   (CTInt _, Number num)
                       | Just int <- toBoundedInteger num -> pure $ CTInt int
@@ -78,5 +73,5 @@ fillConfigItems supported fields = mapM fillConfigItem supported
                   (CTBool _, Bool b) -> pure $ CTBool b
                   (CTEnum vars _, String s)
                       | s `elem` vars -> pure $ CTEnum vars s
-                  _ -> throwError $ inject $ IncompatibleValue name typ yamlVal
+                  _ -> throwError $ IncompatibleValue name typ yamlVal
       pure ConfigItem { name = name, typ = val }
