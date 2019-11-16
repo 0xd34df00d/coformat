532eec11fd91b4525fb86f231e23b1026fedc3b6
0xd34df00d <0xd34df00d@gmail.com>
2019-11-16 09:13:43 -0500

    Factor out our lifted forConcurrently'

--- src/Clang/Coformat/Optimization.hs
+++ src/Clang/Coformat/Optimization.hs
@@ -1,6 +1,6 @@
-{-# LANGUAGE GADTs, DataKinds #-}
+{-# LANGUAGE GADTs, DataKinds, TypeApplications, RankNTypes #-}
 {-# LANGUAGE FlexibleInstances, FlexibleContexts #-}
-{-# LANGUAGE QuasiQuotes, RecordWildCards #-}
+{-# LANGUAGE QuasiQuotes, RecordWildCards, LambdaCase #-}
 
 module Clang.Coformat.Optimization where
 
@@ -20,17 +20,24 @@ import Clang.Coformat.Util
 import Clang.Format.Descr
 import Text.Levenshteins
 
+forConcurrently' :: (MonadLoggerIO m, MonadError e m)
+                 => [a]
+                 -> (forall m'. (MonadLoggerIO m', MonadError e m') => a -> m' b)
+                 -> m [b]
+forConcurrently' lst act = do
+  logger <- askLoggerIO
+  result <- liftIO $ forConcurrently lst $ \elt -> flip runLoggingT logger $ runExceptT $ act elt
+  liftEither $ sequence result
+
 chooseBaseStyle :: (MonadError String m, MonadLoggerIO m) => [ConfigItemT 'Value] -> [T.Text] -> [String] -> m T.Text
 chooseBaseStyle ufos baseStyles files = do
-  logger <- askLoggerIO
-  estimates <- liftIO $ forConcurrently ((,) <$> baseStyles <*> files) $ \(sty, file) -> flip runLoggingT logger $ runExceptT $ do
+  sty2dists <- forConcurrently' ((,) <$> baseStyles <*> files) $ \(sty, file) -> do
     let formattedSty = formatStyArg StyOpts { basedOnStyle = sty, overriddenOpts = ufos }
     stdout <- checked [sh|clang-format --style="#{formattedSty}" #{file}|]
     source <- liftIO $ readFile file
     let dist = levenshteinDistanceWith (blindTokens . dropStartSpaces) source $ TL.unpack stdout
     logDebugN [i|Initial guess for #{sty} at #{file}: #{dist}|]
     pure (sty, dist)
-  sty2dists <- liftEither $ sequence estimates
   let accumulated = HM.toList $ HM.fromListWith (+) sty2dists
   forM_ accumulated $ \(sty, acc) -> logInfoN [i|Initial accumulated guess for #{sty}: #{acc}|]
   pure $ fst $ minimumBy (comparing snd) accumulated
