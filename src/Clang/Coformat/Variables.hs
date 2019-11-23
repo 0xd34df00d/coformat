43a805bf57ec6691a5d6c43bf1c9fed960d6b873
0xd34df00d <0xd34df00d@gmail.com>
2019-11-23 18:28:53 -0500

    More generic Variate

--- src/Clang/Coformat/Variables.hs
+++ src/Clang/Coformat/Variables.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE GADTs, DataKinds, TypeApplications #-}
+{-# LANGUAGE GADTs, TypeFamilies, DataKinds, TypeApplications, ConstraintKinds #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE LambdaCase #-}
 
@@ -10,19 +10,24 @@ import Control.Monad
 
 import Clang.Format.Descr
 
-class CategoricalVariate a where
-  variate :: a -> [a]
+class Variate a where
+  type VariateResult a :: * -> *
+  variate :: a -> VariateResult a a
   varPrism :: Prism' (ConfigTypeT 'Value) a
 
-instance CategoricalVariate Bool where
+instance Variate Bool where
+  type VariateResult Bool = []
   variate b = [not b]
   varPrism = prism' CTBool $ \case CTBool b -> Just b
                                    _ -> Nothing
 
-instance CategoricalVariate ([T.Text], T.Text) where
+instance Variate ([T.Text], T.Text) where
+  type VariateResult ([T.Text], T.Text) = []
   variate (vars, cur) = [(vars, next) | next <- vars, next /= cur]
   varPrism = prism' (uncurry CTEnum) $ \case CTEnum vars cur -> Just (vars, cur)
                                              _ -> Nothing
+
+type CategoricalVariate a = (Variate a, VariateResult a ~ [])
 
 data CategoricalVariable where
   -- TODO proxy should be enough
