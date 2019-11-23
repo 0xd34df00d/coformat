fca1d1edd82f2292cf6d6bfe1b7294d8afe37399
0xd34df00d <0xd34df00d@gmail.com>
2019-11-23 18:44:40 -0500

    Added integral variables/variates

--- src/Clang/Coformat/Variables.hs
+++ src/Clang/Coformat/Variables.hs
@@ -7,6 +7,7 @@ module Clang.Coformat.Variables where
 import qualified Data.Text as T
 import Control.Lens
 import Control.Monad
+import Numeric.Natural
 
 import Clang.Format.Descr
 
@@ -46,3 +47,31 @@ typToDV val = msum [ MkDV <$> val ^? varPrism @Bool
                    , MkDV <$> val ^? varPrism @([T.Text], T.Text)
                    ]
 
+
+data IntegralVariateResult a
+  = None
+  | SingleDirection a
+  | TwoDirections a a
+  deriving (Eq, Show)
+
+instance Variate Int where
+  type VariateResult Int = IntegralVariateResult
+  variate n = TwoDirections (n - 1) (n + 1)
+  varPrism = prism' CTInt $ \case CTInt n -> Just n
+                                  _ -> Nothing
+
+instance Variate Natural where
+  type VariateResult Natural = IntegralVariateResult
+  variate n | n > 0 = TwoDirections (n - 1) (n + 1)
+            | otherwise = SingleDirection (n + 1)
+  varPrism = prism' CTUnsigned $ \case CTUnsigned n -> Just n
+                                       _ -> Nothing
+
+type IntegralVariate a = (Variate a, VariateResult a ~ IntegralVariateResult)
+type IntegralVariable = Variable IntegralVariateResult
+type IxedIntegralVariable = IxedVariable IntegralVariateResult
+
+typToIV :: ConfigTypeT 'Value -> Maybe IntegralVariable
+typToIV val = msum [ MkDV <$> val ^? varPrism @Int
+                   , MkDV <$> val ^? varPrism @Natural
+                   ]
