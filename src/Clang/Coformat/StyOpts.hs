0f4d6087d55469acccdfaff27036bfd6aaee404e
0xd34df00d <0xd34df00d@gmail.com>
2019-11-28 16:29:30 -0500

    A proper toJSON instance for StyOpts

--- src/Clang/Coformat/StyOpts.hs
+++ src/Clang/Coformat/StyOpts.hs
@@ -21,21 +21,24 @@ data StyOpts = StyOpts
   , additionalOpts :: [ConfigItemT 'Value]
   }
 
+instance ToJSON StyOpts where
+  toJSON StyOpts { .. } = Object $ foldl' f (HM.singleton "BasedOnStyle" $ String basedOnStyle) additionalOpts
+    where
+      f hm ConfigItem { .. } = updateRec typ name hm
+
+      updateRec typ [key] = HM.insert key (toJson typ)
+      updateRec typ (key:rest) = HM.alter (Just . Object . updateRec typ rest . getObj) key
+      updateRec _ [] = id
+      getObj = fromMaybe mempty . ((^? _Object) =<<)
+
+      toJson (CTInt n) = Number $ fromIntegral n
+      toJson (CTUnsigned n) = Number $ fromIntegral n
+      toJson (CTString v) = absurd v
+      toJson (CTStringVec v) = absurd v
+      toJson (CTRawStringFormats v) = absurd v
+      toJson (CTIncludeCats v) = absurd v
+      toJson (CTBool b) = Bool b
+      toJson (CTEnum _ opt) = String opt
+
 formatStyArg :: StyOpts -> BSL.ByteString
-formatStyArg StyOpts { .. } = encode $ Object $ foldl' f (HM.singleton "BasedOnStyle" $ String basedOnStyle) additionalOpts
-  where
-    f hm ConfigItem { .. } = updateRec typ name hm
-
-    updateRec typ [key] = HM.insert key (toJson typ)
-    updateRec typ (key:rest) = HM.alter (Just . Object . updateRec typ rest . getObj) key
-    updateRec _ [] = id
-    getObj = fromMaybe mempty . ((^? _Object) =<<)
-
-    toJson (CTInt n) = Number $ fromIntegral n
-    toJson (CTUnsigned n) = Number $ fromIntegral n
-    toJson (CTString v) = absurd v
-    toJson (CTStringVec v) = absurd v
-    toJson (CTRawStringFormats v) = absurd v
-    toJson (CTIncludeCats v) = absurd v
-    toJson (CTBool b) = Bool b
-    toJson (CTEnum _ opt) = String opt
+formatStyArg = encode . toJSON
