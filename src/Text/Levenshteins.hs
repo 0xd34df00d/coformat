80efe376a565d5f7936a705f876f836290c8f33d
0xd34df00d <0xd34df00d@gmail.com>
2019-11-30 17:08:21 -0500

    Vector.Unboxed.Mutable lev dist implementation

--- src/Text/Levenshteins.hs
+++ src/Text/Levenshteins.hs
@@ -7,7 +7,10 @@ module Text.Levenshteins
 , levenshteinDistanceWith
 ) where
 
+import qualified Data.ByteString.Char8 as BS
 import qualified Data.Sequence as S
+import qualified Data.Vector.Unboxed.Mutable as VUM
+import Control.Monad.ST
 import Data.Char
 import Data.Foldable
 import Data.Sequence(Seq(..))
@@ -33,3 +36,28 @@ levenshteinDistanceWith f s1 s2 = levenshteinDistance defaultEditCosts (f s1) (f
 
 leaveStartSpaces :: StringNormalizer
 leaveStartSpaces = unlines . map (takeWhile isSpace) . lines
+
+levenshteinDistance' :: BS.ByteString -> BS.ByteString -> Int
+levenshteinDistance' s1 s2 = runST $ do
+  v0Init <- VUM.unsafeNew $ n + 1
+  forM_ [0..n] $ \i -> VUM.unsafeWrite v0Init i i
+  v1Init <- VUM.unsafeNew $ n + 1
+  v0 <- loop 0 v0Init v1Init
+  VUM.unsafeRead v0 n
+  where
+    m = BS.length s1
+    n = BS.length s2
+
+    loop i v0 v1
+      | i == m = pure v0
+      | otherwise = do
+          VUM.unsafeWrite v1 0 (i + 1)
+          forM_ [0..n - 1] $ \j -> do
+            delCost <- inc $ v0 `VUM.unsafeRead` (j + 1)
+            insCost <- inc $ v1 `VUM.unsafeRead` j
+            substCost <- if s1 `BS.index` i == s2 `BS.index` j
+                         then v0 `VUM.unsafeRead` j
+                         else inc $ v0 `VUM.unsafeRead` j
+            VUM.unsafeWrite v1 (j + 1) $ minimum [delCost, insCost, substCost]
+          loop (i + 1) v1 v0
+      where inc = fmap (1 +)
