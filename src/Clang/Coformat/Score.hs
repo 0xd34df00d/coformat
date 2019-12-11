b84be0d836db2f6b087f25e2f1d7c11e6eac5daf
0xd34df00d <0xd34df00d@gmail.com>
2019-12-11 17:56:12 -0500

    A totally different way of scoring mismatches
    Don't base on Levenshtein distance, but use significant letters counts and
    spaces mismatches.

--- src/Clang/Coformat/Score.hs
+++ src/Clang/Coformat/Score.hs
@@ -1,16 +1,49 @@
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving, DerivingStrategies, DerivingVia #-}
+{-# LANGUAGE DeriveGeneric, DerivingVia #-}
+{-# LANGUAGE RecordWildCards #-}
 
-module Clang.Coformat.Score where
+module Clang.Coformat.Score
+( Score
+, calcScore
+) where
 
 import qualified Data.ByteString.Char8 as BS
+import qualified Data.HashMap.Strict as HM
+import Data.Char
 import Data.Monoid
+import GHC.Generics
+import Generic.Data
 
-import Text.Levenshteins
-
-newtype Score = Score { getScore :: Int } deriving (Eq, Ord, Show)
-                                          deriving newtype (Bounded)
-                                          deriving (Semigroup, Monoid) via (Sum Int)
+data Score = Score
+  { meaningfulLettersCountsDiff :: Sum Int
+  , spacesCountsDiff :: Sum Int
+  , spacesMisalignment :: Sum Int
+  } deriving (Eq, Ord, Show, Bounded, Generic)
+    deriving (Semigroup, Monoid) via (Generically Score)
 
 calcScore :: BS.ByteString -> BS.ByteString -> Score
-calcScore s1 s2 = Score $ levenshteinDistanceWith id (BS.unpack s1) (BS.unpack s2)
+calcScore s1 s2 = Score { .. }
+  where
+    meaningfulLettersCountsDiff = Sum $ calcLettersDiff (not . isSpace) s1 s2
+    spacesCountsDiff = Sum $ calcLettersDiff (const True) s1 s2
+    spacesMisalignment | meaningfulLettersCountsDiff /= 0 = 0
+                       | otherwise = Sum $ alignSpaces s1 s2
+
+calcLettersDiff :: (Char -> Bool) -> BS.ByteString -> BS.ByteString -> Int
+calcLettersDiff flt s1 s2 = sum $ HM.elems $ HM.unionWith (\v1 v2 -> abs $ v1 - v2) s1counters s2counters
+  where
+    calcLetters = BS.foldl' ins mempty
+    ins hm ch | flt ch = HM.insertWith (+) ch 1 hm
+              | otherwise = hm
+    s1counters = calcLetters s1
+    s2counters = calcLetters s2
+
+alignSpaces :: BS.ByteString -> BS.ByteString -> Int
+alignSpaces bs1 bs2 = go (BS.unpack bs1) (BS.unpack bs2)
+  where
+    go s1 [] = length s1
+    go [] s2 = length s2
+    go (c1:s1) (c2:s2) | c1 == c2 = go s1 s2
+                       | isSpace c1 && isSpace c2 = 2 + go s1 s2
+                       | isSpace c1 = 1 + go s1 (c2:s2)
+                       | isSpace c2 = 1 + go (c1:s1) s2
+                       | otherwise = 2 + length s1 + length s2
