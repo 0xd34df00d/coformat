7a05278f94f75b9a01790d7ab8dda288aa4dc780
0xd34df00d <0xd34df00d@gmail.com>
2019-12-15 14:29:04 -0500

    Lift ValueNotFound error to a separate type

--- src/Clang/Format/YamlConversions.hs
+++ src/Clang/Format/YamlConversions.hs
@@ -27,30 +27,33 @@ import Clang.Coformat.StyOpts
 data FillError
   = YamlParseError ParseException
   | YamlAnalysisError YamlAnalysisError
-  deriving (Show, Generic, CoHas ParseException, CoHas YamlAnalysisError)
+  | YamlValueNotFound ValueNotFound
+  deriving (Show, Generic, CoHas ParseException, CoHas YamlAnalysisError, CoHas ValueNotFound)
 
-preprocessYaml :: (MonadError e m, CoHas ParseException e, CoHas YamlAnalysisError e)
+preprocessYaml :: (MonadError e m, CoHas ParseException e, CoHas YamlAnalysisError e, CoHas ValueNotFound e)
                => BS.ByteString -> m Object
 preprocessYaml yamlContents = liftEither (decodeEither' yamlContents)
                           >>= extractMap
                           >>= braceWrappingKludge
 
-fillConfigItems :: (MonadError e m, CoHas ParseException e, CoHas YamlAnalysisError e)
+fillConfigItems :: (MonadError e m, CoHas ParseException e, CoHas YamlAnalysisError e, CoHas ValueNotFound e)
                 => [ConfigItemT 'Supported] -> BS.ByteString -> m [ConfigItemT 'Value]
 fillConfigItems supported yamlContents = preprocessYaml yamlContents
                                      >>= fillConfigItemsFromObj supported
+                                     >>= liftEither . sequence
 
 data YamlAnalysisError
   = YamlNotAnObject String
-  | ValueNotFound T.Text
   | IncompatibleValue T.Text (ConfigTypeT 'Supported) Value
   deriving (Show)
+
+newtype ValueNotFound = ValueNotFound { missingValueName :: T.Text } deriving (Show)
 
 extractMap :: (MonadError e m, CoHas YamlAnalysisError e) => Value -> m Object
 extractMap (Object fields) = pure fields
 extractMap _ = throwError $ YamlNotAnObject "Top-level value is not an object"
 
-braceWrappingKludge :: (MonadError e m, CoHas YamlAnalysisError e) => Object -> m Object
+braceWrappingKludge :: (MonadError e m, CoHas YamlAnalysisError e, CoHas ValueNotFound e) => Object -> m Object
 braceWrappingKludge fields = do
   bwVal <- liftMaybe (ValueNotFound bwField) $ HM.lookup bwField fields
   bwValObj <- case bwVal of
@@ -64,28 +67,28 @@ braceWrappingKludge fields = do
     bwField = "BraceWrapping"
 
 fillConfigItemsFromObj :: (MonadError e m, CoHas YamlAnalysisError e)
-                       => [ConfigItemT 'Supported] -> Object -> m [ConfigItemT 'Value]
+                       => [ConfigItemT 'Supported] -> Object -> m [Either ValueNotFound (ConfigItemT 'Value)]
 fillConfigItemsFromObj supported fields = mapM fillConfigItem supported
   where
-    fillConfigItem ConfigItem { .. } = do
-      yamlVal <- case HM.lookup nameConcatted fields of
-                      Just val -> pure val
-                      Nothing -> throwError $ ValueNotFound nameConcatted
-      val <- case (typ, yamlVal) of
-                  (CTInt _, Number num)
-                      | Just int <- toBoundedInteger num -> pure $ CTInt int
-                  (CTUnsigned _, Number num)
-                      | Just int <- toBoundedInteger num :: Maybe Int
-                      , int >= 0 -> pure $ CTUnsigned $ fromIntegral int
-                  (CTBool _, Bool b) -> pure $ CTBool b
-                  (CTEnum vars _, String s)
-                      | s `elem` vars -> pure $ CTEnum vars s
-                  (CTEnum vars _, Bool b)
-                      | boolAsEnumVar b `elem` vars -> pure $ CTEnum vars $ boolAsEnumVar b
-                  _ -> throwError $ IncompatibleValue nameConcatted typ yamlVal
-      pure ConfigItem { name = name, typ = val }
+    fillConfigItem ConfigItem { .. }
+      | Nothing <- maybeYamlVal = pure $ Left $ ValueNotFound nameConcatted
+      | Just yamlVal <- maybeYamlVal = do
+        val <- case (typ, yamlVal) of
+                    (CTInt _, Number num)
+                        | Just int <- toBoundedInteger num -> pure $ CTInt int
+                    (CTUnsigned _, Number num)
+                        | Just int <- toBoundedInteger num :: Maybe Int
+                        , int >= 0 -> pure $ CTUnsigned $ fromIntegral int
+                    (CTBool _, Bool b) -> pure $ CTBool b
+                    (CTEnum vars _, String s)
+                        | s `elem` vars -> pure $ CTEnum vars s
+                    (CTEnum vars _, Bool b)
+                        | boolAsEnumVar b `elem` vars -> pure $ CTEnum vars $ boolAsEnumVar b
+                    _ -> throwError $ IncompatibleValue nameConcatted typ yamlVal
+        pure $ Right $ ConfigItem { name = name, typ = val }
       where
         nameConcatted = T.intercalate "." name
+        maybeYamlVal = HM.lookup nameConcatted fields
 
 formatClangFormat :: StyOpts -> BS.ByteString
 formatClangFormat = encodePretty $ setConfCompare compare defConfig
