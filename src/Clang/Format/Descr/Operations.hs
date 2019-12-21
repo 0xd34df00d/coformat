7de608290d033bdc1d7893091fb50797462ed0f3
0xd34df00d <0xd34df00d@gmail.com>
2019-12-21 18:12:30 -0500

    Better error message in case of parse failure

--- src/Clang/Format/Descr/Operations.hs
+++ src/Clang/Format/Descr/Operations.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE DataKinds, GADTs #-}
-{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleContexts, ScopedTypeVariables #-}
 {-# LANGUAGE RecordWildCards, LambdaCase, QuasiQuotes, OverloadedStrings #-}
 
 module Clang.Format.Descr.Operations where
@@ -8,8 +8,10 @@ import qualified Data.HashMap.Strict as HM
 import qualified Data.Map.Strict as M
 import qualified Data.Text as T
 import Control.Monad.Except
+import Data.Bifunctor
 import Data.Maybe
 import Data.String.Interpolate.IsString
+import Data.Typeable
 import Data.Void
 import Text.Read
 
@@ -39,19 +41,21 @@ subtractMatching minuend subtrahend = filter f minuend
     f ConfigItem { .. } = (/= Just value) $ HM.lookup name subMap
     subMap = HM.fromList [ (name, value) | ConfigItem { .. } <- subtrahend]
 
-parseConfigValue :: MonadError String m => (CTData f Void ~ Void) => ConfigItemT f -> String -> m (ConfigItemT 'Value)
+parseConfigValue :: (MonadError String m, CTData f Void ~ Void, Show (ConfigTypeT f)) => ConfigItemT f -> String -> m (ConfigItemT 'Value)
 parseConfigValue cfg str = liftEither $ (\parsed -> ConfigItem { name = name cfg, value = parsed }) <$> eitherParsed
   where
     eitherParsed = case value cfg of
-                        CTInt _ -> CTInt <$> readEither str
-                        CTUnsigned _ -> CTUnsigned <$> readEither str
-                        CTBool _ -> CTBool <$> readEither str
+                        CTInt _ -> CTInt <$> readEither'
+                        CTUnsigned _ -> CTUnsigned <$> readEither'
+                        CTBool _ -> CTBool <$> readEither'
                         CTString val -> absurd val
                         CTStringVec val -> absurd val
                         CTRawStringFormats val -> absurd val
                         CTIncludeCats val -> absurd val
                         CTEnum variants _ -> do
-                          var <- readEither str
+                          var <- readEither'
                           unless (var `elem` variants) $
                             throwError [i|Unsupported option `${var}`, supported ones are `#{T.intercalate "`, `" variants}`|]
                           pure $ CTEnum variants var
+    readEither' :: forall a. (Typeable a, Read a) => Either String a
+    readEither' = first (\err -> [i|Error parsing #{str} as #{typeRep (Proxy :: Proxy a)}: #{err}, expected type: #{value cfg}|]) $ readEither str
