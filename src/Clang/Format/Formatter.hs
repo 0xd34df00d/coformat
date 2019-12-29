20c3563107adddaa68f0ab5f8bf019fe26363363
0xd34df00d <0xd34df00d@gmail.com>
2019-12-29 13:37:06 -0500

    Stub implementation of the clang formatter

---
+++ src/Clang/Format/Formatter.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE RecordWildCards, QuasiQuotes, OverloadedStrings #-}
+
+module Clang.Format.Formatter(clangFormatter) where
+
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.ByteString.Lazy.Char8 as LBS
+import Control.Monad.Except
+import Data.Bifunctor
+import Data.List
+import Data.String.Interpolate
+
+import Language.Coformat.Formatter
+import Clang.Coformat.StyOpts
+import Clang.Format.Descr
+import Clang.Format.Descr.Operations
+import Clang.Format.DescrParser
+
+clangFormatter :: Formatter
+clangFormatter = Formatter { .. }
+  where
+    formatterInfo = FormatterInfo { .. }
+    executableName = "clang-format"
+    formatterOpts = OptsFromFile "data/ClangFormatStyleOptions-9.html" parseOptsDescription
+
+    formatFile baseSty opts path = Cmd { exec = executableName, args = [ "--style=" <> formattedBaseSty, BS.pack path ] }
+      where
+        formattedBaseSty = formatStyArg $ StyOpts { basedOnStyle = baseSty, additionalOpts = opts }
+
+liftEither' :: (MonadError String m, Show e) => String -> Either e a -> m a
+liftEither' context = liftEither . first ((context <>) . show)
+
+parseOptsDescription :: LBS.ByteString -> Either String OptsDescription
+parseOptsDescription contents = do
+  parseResult <- liftEither' "Unable to parse the file: " $ parseDescr contents
+  let supportedOptions = filterParsedItems parseResult
+  baseStyles <- case find ((== bosKey) . name) supportedOptions of
+                     Nothing -> throwError "No `BasedOnStyle` option"
+                     Just stys -> pure stys
+  let varyingOptions = filter ((/= bosKey) . name) supportedOptions
+  case value baseStyles of
+       CTEnum { .. } -> pure $ OptsDescription { baseStyles = variants, knownOpts = varyingOptions }
+       _ -> throwError [i|Unknown type for the `BaseStyles` option: #{value baseStyles}|]
+  where
+    bosKey = ["BasedOnStyle"]
