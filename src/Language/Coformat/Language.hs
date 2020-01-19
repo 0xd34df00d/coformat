dbf573d505e7293bc75d357539bb8e170425726a
0xd34df00d <0xd34df00d@gmail.com>
2020-01-19 14:06:28 -0500

    Actual language guessing

--- src/Language/Coformat/Language.hs
+++ src/Language/Coformat/Language.hs
@@ -1,4 +1,25 @@
+{-# LANGUAGE TupleSections #-}
+
 module Language.Coformat.Language where
 
-data Language = C | Cpp
+import qualified Data.HashMap.Strict as HM
+import Data.List.NonEmpty
+import Data.Maybe
+import System.FilePath.Posix
+
+data Language = Cpp
   deriving (Eq, Ord, Show)
+
+guessLanguage :: NonEmpty FilePath -> Either String Language
+guessLanguage files = case langs of
+                           [] -> Left "Could not detect any languages"
+                           [lang] -> Right lang
+                           _ -> Left "More than one language detected"
+  where
+    langs = mapMaybe extToFile $ toList $ takeExtension <$> files
+
+extToFile :: String -> Maybe Language
+extToFile ext = HM.lookup ext exts
+  where
+    exts = HM.fromList $ (, Cpp) <$> cpps
+    cpps = ["cpp", "c", "h", "cxx", "hpp", "hxx"]
